16cfa0293cdb96afc6e85b81052aa3d44b720a1f
Edu <efercor3@insbabrielamistral.cat>
2016-11-15 16:49:16 +0100

    rv

--- TeleDASis/TeleDASis/Form1.Designer.cs
+++ TeleDASis/TeleDASis/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(155, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(429, 329);
+            this.pictureBox1.Size = new System.Drawing.Size(146, 145);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
